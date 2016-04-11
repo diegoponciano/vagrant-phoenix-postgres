b6ef24d59012c636f2afd72d7493d7ae760833a2
Rudolf Bargholz <rudolf@bargholz.ch>
2016-04-11 21:06:52 +0200

    Phoenix was not being installed correctly

--- config/vagrant/phoenix_setup.sh
+++ config/vagrant/phoenix_setup.sh
@@ -7,7 +7,7 @@ echo "=== Begin Vagrant Provisioning using 'config/vagrant/phoenix_setup.sh'"
 
 # Install the latest version of Phoenix
   echo "===== Installing Phoenix"
-  yes Y | mix local.hex
-  yes Y | mix archive.install "https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez"
+  mix local.hex --force
+  mix archive.install "https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez" --force
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/phoenix_setup.sh'"

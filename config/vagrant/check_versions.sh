110d927298764a0e48ce23cb2779f99241aae920
Rudolf Bargholz <rudolf@bargholz.ch>
2016-04-11 21:07:41 +0200

    Added test for Phoenix install
    Added empty lines between tests to make the result a little easier to
    read

--- config/vagrant/check_versions.sh
+++ config/vagrant/check_versions.sh
@@ -11,14 +11,19 @@ echo ""
 echo "node --version"
 node --version
 echo ""
+echo ""
 echo "psql --version"
 psql --version
 echo ""
 echo "cat /usr/lib/erlang/releases/RELEASES"
 cat /usr/lib/erlang/releases/RELEASES
 echo ""
+echo ""
 echo "elixir --version"
 elixir --version
+echo ""
+echo "mix help phoenix.new"
+mix help phoenix.new
 echo ""
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/check_version.sh'"

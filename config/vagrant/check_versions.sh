c3896bdeb536af7746dd3944c5ab0e8eb8b91292
Rudolf Bargholz <rudolf@bargholz.ch>
2016-04-11 21:44:13 +0200

    Clean up the display of the version display

--- config/vagrant/check_versions.sh
+++ config/vagrant/check_versions.sh
@@ -5,26 +5,32 @@ echo "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 echo "======================================================================"
 echo "=== Begin Vagrant Provisioning using 'config/vagrant/check_version.sh'"
 
+echo " "
+echo " "
 echo "git --version"
 git --version
-echo ""
+echo " "
+echo " "
 echo "node --version"
 node --version
-echo ""
-echo ""
+echo " "
+echo " "
 echo "psql --version"
 psql --version
-echo ""
+echo " "
+echo " "
 echo "cat /usr/lib/erlang/releases/RELEASES"
 cat /usr/lib/erlang/releases/RELEASES
 echo " "
 echo " "
 echo "elixir --version"
 elixir --version
-echo ""
+echo " "
+echo " "
 echo "mix help phoenix.new"
 mix help phoenix.new | sed -e 3b -e '$!d'
 strings ~/.mix/archives/phoenix_new.ez | grep '{vsn,'
-echo ""
+echo " "
+echo " "
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/check_version.sh'"

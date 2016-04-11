9a8304b8e9d618f90a12db835832ab9b20a3b52a
Rudolf Bargholz <rudolf@bargholz.ch>
2016-04-11 21:07:08 +0200

    Removed duplicate Phoenix install

--- config/vagrant/elixir_setup.sh
+++ config/vagrant/elixir_setup.sh
@@ -22,8 +22,6 @@ if [ -z `which elixir` ]; then
   sudo apt-get update
   yes Y | sudo apt-get install esl-erlang
   sudo apt-get install elixir
-  yes Y | mix local.hex
-  yes Y | mix archive.install "https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez"
 fi
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/elixir_setup.sh'"

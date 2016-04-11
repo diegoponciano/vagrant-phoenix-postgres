1e2815065bf2cc1991c874d8588ccb829f0c1e6c
Rudolf Bargholz <rudolf@bargholz.ch>
2016-04-11 16:46:47 +0200

    No erlang dist version available specifically for xenial
    Hard code wily

--- config/vagrant/elixir_setup.sh
+++ config/vagrant/elixir_setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
+echo "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 echo "======================================================================"
 echo "=== Begin Vagrant Provisioning using 'config/vagrant/elixir_setup.sh'"
 
@@ -14,7 +16,9 @@ if [ -z `which elixir` ]; then
   # prompted, and used for the insall, and the installation of erlang fails.
   wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
   sudo apt-key add erlang_solutions.asc
-  sudo add-apt-repository "deb http://packages.erlang-solutions.com/ubuntu $(lsb_release -s -c) contrib"
+  # There is as of now no erlang download for xenial, so we have to hard code wily.
+  sudo add-apt-repository "deb http://packages.erlang-solutions.com/ubuntu wily contrib"
+  # sudo add-apt-repository "deb http://packages.erlang-solutions.com/ubuntu $(lsb_release -s -c) contrib"
   sudo apt-get update
   yes Y | sudo apt-get install esl-erlang
   sudo apt-get install elixir

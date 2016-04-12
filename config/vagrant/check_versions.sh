4d2c3efd64f2d599dd50acc734de5ed888ce1bb9
Rudolf Bargholz <rudolf@bargholz.ch>
2016-04-12 13:09:07 +0200

    Improved the test for phoenix installation
    Cosmetic change to delimit tests and make clear which software is being
    tested.

--- config/vagrant/check_versions.sh
+++ config/vagrant/check_versions.sh
@@ -7,29 +7,45 @@ echo "=== Begin Vagrant Provisioning using 'config/vagrant/check_version.sh'"
 
 echo " "
 echo " "
+echo "--- git"
+echo "--------------------"
 echo "git --version"
 git --version
 echo " "
 echo " "
+echo "--- node"
+echo "--------------------"
 echo "node --version"
 node --version
 echo " "
 echo " "
+echo "--- postgres"
+echo "--------------------"
 echo "psql --version"
 psql --version
 echo " "
 echo " "
+echo "--- erlang"
+echo "--------------------"
 echo "cat /usr/lib/erlang/releases/RELEASES"
 cat /usr/lib/erlang/releases/RELEASES
 echo " "
 echo " "
+echo "--- elixir"
+echo "--------------------"
 echo "elixir --version"
 elixir --version
 echo " "
 echo " "
+echo "--- phoenix"
+echo "--------------------"
 echo "mix help phoenix.new"
-mix help phoenix.new | sed -e 3b -e '$!d'
-strings ~/.mix/archives/phoenix_new.ez | grep '{vsn,'
+if [ $(strings ~/.mix/archives/phoenix_new.ez | grep -co '{vsn,') = 0 ]; then
+  echo "Error: unable to find installed phoenix"
+else
+  mix help phoenix.new | sed -e 3b -e '$!d'
+  strings ~/.mix/archives/phoenix_new.ez | grep '{vsn,'
+fi
 echo " "
 echo " "
 

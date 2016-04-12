c18907e4137f73a0195df9cfbe5f5980d2b626f5
Rudolf Bargholz <rudolf@bargholz.ch>
2016-04-12 09:54:16 +0200

    Moved installation of hex and rebar into the Elixir script
    For most Phoenix projects, or Elixir projects in general at least hex
    needs to be installed. And more often than not a project relies on
    Erlang packages that need rebar, so it might as well be installed
    proactively.

--- config/vagrant/phoenix_setup.sh
+++ config/vagrant/phoenix_setup.sh
@@ -7,7 +7,6 @@ echo "=== Begin Vagrant Provisioning using 'config/vagrant/phoenix_setup.sh'"
 
 # Install the latest version of Phoenix
   echo "===== Installing Phoenix"
-  mix local.hex --force
   mix archive.install "https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez" --force
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/phoenix_setup.sh'"

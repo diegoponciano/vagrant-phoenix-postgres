3d9f0b03ca58cd3e7c25376809a97cbd449d5d7e
Rudolf Bargholz <rudolf@bargholz.ch>
2016-04-12 10:10:44 +0200

    Corrected file names in echo scripts
    Also removed the duplicate install command for elm from the user script.
    
    npm install needs to be prefixed prefixed by sudo.

--- config/vagrant/post_install_priveleged.sh
+++ config/vagrant/post_install_priveleged.sh
@@ -2,12 +2,12 @@
 echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
 echo "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 echo "======================================================================"
-echo "=== Begin Vagrant Provisioning using 'config/vagrant/post_install.sh'"
+echo "=== Begin Vagrant Provisioning using 'config/vagrant/post_install_priveleged.sh'"
 
 # Here I can place post install commands to install stuff which I need for
-# my project. This file is specifically for code that must be 
+# my project. This file is specifically for code that must be
 # executed as the super-user.
 
-npm install -g elm
+sudo npm install -g elm
 
-echo "=== End Vagrant Provisioning using 'config/vagrant/post_install.sh'"
+echo "=== End Vagrant Provisioning using 'config/vagrant/post_install_priveleged.sh'"
--- config/vagrant/post_install_user.sh
+++ config/vagrant/post_install_user.sh
@@ -2,11 +2,10 @@
 echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
 echo "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 echo "======================================================================"
-echo "=== Begin Vagrant Provisioning using 'config/vagrant/post_install_priveleged.sh'"
+echo "=== Begin Vagrant Provisioning using 'config/vagrant/post_install_user.sh'"
 
 # Here I can place post install commands to install stuff which I need for
 # my project.
 
-npm install -g elm
 
-echo "=== End Vagrant Provisioning using 'config/vagrant/post_install_priveleged.sh'"
+echo "=== End Vagrant Provisioning using 'config/vagrant/post_install_user.sh'"
